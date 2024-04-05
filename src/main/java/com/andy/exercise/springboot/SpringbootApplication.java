5bc0d901c7d393d7e35508eaa8db01a23bd0fbf6
Andy <thoaitran93@gmail.com>
2024-04-05 23:13:30 +0700

    init springboot project

---
+++ src/main/java/com/andy/exercise/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.andy.exercise.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andy/exercise/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.exercise.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
